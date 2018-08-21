df921b9cfc45d456504b75f074a2cc5180a5d344
Pfeifer, Denzel <dpfeifr2@ug-mac33.ics.illinois.edu>
2018-08-21 16:09:45 -0500

    Update

--- FUT Accountant/SecondViewController.swift
+++ FUT Accountant/SecondViewController.swift
@@ -9,8 +9,30 @@
 import UIKit
 
 class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+
+    @IBOutlet weak var wlswitch: UISwitch!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var osswitch: UISwitch!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        if wlswitch.isOn {
+            
+        }
+    }
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self
+        picker.dataSource = self
+        
+        pickerData = ["Division 1", "Division 2", "Division 3", "Division 4", "Division 5", "Division 6", "Division 7", "Division 8", "Division 9", "Division 10"]
+    }
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 10;
+        return 1;
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -20,18 +42,7 @@ class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return pickerData[row]
     }
-
-    @IBOutlet weak var picker: UIPickerView!
     
-    var pickerData: [String] = [String]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        pickerData = ["Division 1", "Division 2", "Division 3", "Division 4", "Division 5", "Division 6", "Division 7", "Division 8", "Division 9", "Division 10"]
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
