291e60bfe9fa4c63b33799be45fd61f97f36b9b0
Pfeifer, Denzel <dpfeifr2@ug-mac33.ics.illinois.edu>
2018-08-21 15:44:20 -0500

    Second viewcontroller

--- FUT Accountant/SecondViewController.swift
+++ FUT Accountant/SecondViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 10;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
 
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pickerData = ["Division 1", "Division 2", "Division 3", "Division 4", "Division 5", "Division 6", "Division 7", "Division 8", "Division 9", "Division 10"]
     }
 
     override func didReceiveMemoryWarning() {
