a9ba44a7affd31086365ac66f0caacd66f9aab7f
Pfeifer, Denzel <dpfeifr2@ug-mac33.ics.illinois.edu>
2018-08-21 13:13:26 -0500

    Initial Commit

---
+++ FUT Accountant/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FUT Accountant
+//
+//  Created by Pfeifer, Denzel on 8/21/18.
+//  Copyright © 2018 Pfeifer, Denzel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
